3c51f78cbc63b525d49052324932f6212284d225
Varun Shukla <varunshukla736@gmail.com>
2023-11-25 18:54:01 +0530

    axios1.0

--- src/pages/GetApiAxios.js
+++ src/pages/GetApiAxios.js
@@ -5,28 +5,56 @@ import ImageCard from "../components/common/ImageCard";
 function GetApiAxios() {
   const [data, setdata] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/photos").then((res) => {
-      console.log(res.data);
-      setdata(res.data);
-      setIsLoading(false);
-    });
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/photos"
+        );
+        setdata(response.data);
+      } catch (error) {
+        setError(error);
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
 
-
   if (isLoading) {
-    return <div className="h-screen flex justify-center items-center">Loading...</div>;
+    return (
+      <div className="h-screen flex justify-center items-center">
+        Loading...
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        Error: {error.message}
+      </div>
+    );
+  }
+  if (!data) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        Response: Data not available
+      </div>
+    );
   }
 
   return (
     <div className="mt-4">
+      <div className="my-2">API response using Axios</div>
       <div className="grid lg:grid-cols-4 md:grid-cols-3 gap-4">
         {data.map((d) => {
           return (
             <>
-              <div>
+              <div key={d.id}>
                 <ImageCard title={d.title} url={d.url} id={d.id} />
               </div>
             </>
